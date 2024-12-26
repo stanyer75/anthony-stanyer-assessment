425f237dcae3cd483d908d162e6734c1917cb2c4
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2024-12-26 21:02:14 +0000

    basic view to receive url to minify

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,17 @@
 <x-layout>
-    hello world
+    <div class="flex items-center justify-items-center">
+        <h1 class="text-center">
+            Minify Your Url
+        </h1>
+    </div>
+    <div>
+        <form action="/encode" method="POST">
+            @csrf
+            <label for="url"> url to minify</label>
+            <input type="text" name="url" class="rounded border-black">
+
+            <button type="submit">Minify</button>
+        </form>
+    </div>
+
 </x-layout>
