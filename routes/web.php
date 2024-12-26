98adbc4ac2cab8663be318223f7022026264ed30
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2024-12-26 21:02:27 +0000

    route to post form too

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\EncodeUrlController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/encode', EncodeUrlController::class);
