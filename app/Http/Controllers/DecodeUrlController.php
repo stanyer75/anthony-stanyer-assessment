c8f5ac730a0595c72cfb9b9c1bc2af301a03149d
Anthony Stanyer <121306984+stanyer75@users.noreply.github.com>
2024-12-27 00:03:01 +0000

    add in return type to decode url controller

--- app/Http/Controllers/DecodeUrlController.php
+++ app/Http/Controllers/DecodeUrlController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Url;
+use Illuminate\Http\RedirectResponse;
 use Vinkla\Hashids\Facades\Hashids;
 
 class DecodeUrlController extends Controller
 {
-    public function __invoke(string $url)
+    public function __invoke(string $url): RedirectResponse
     {
         $decodedUrl = Hashids::decode($url);
         $longUrl = Url::find($decodedUrl[0])->longUrl;
